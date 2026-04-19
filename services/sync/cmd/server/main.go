73360dc6e720260d9e450cd1e6b8e884db531134
luketucich <luketucich@gmail.com>
2026-04-19 16:42:45 -0400

    feat(sync): add Go HTTP hello-world server

---
+++ services/sync/cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello from Buresuto sync service!")
+	})
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ services/sync/go.mod
@@ -0,0 +1,3 @@
+module github.com/luketucich/buresuto/services/sync
+
+go 1.25.0
